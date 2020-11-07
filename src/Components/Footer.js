ed51eea3714ebe59dd9d7638977a8178225d565e
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2020-11-07 15:10:57 +0100

    Changed the link route to anchor tag

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,28 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
 	return (
 		<div className="footer container-fluid p-3 p-md-5 bg-blue text-center">
 			<p className="mb-0">
 				Experiencing Technical Difficulty? Call{" "}
-				<Link className="text-red"
-					onClick={() => {
-						window.location.href = `tel:0123456789`;
-					}}
-					to="#"
-				>
+				<a className="text-red" href="tel:0123456789">
 					0123456789
-				</Link>{" "}
+				</a>{" "}
 				or send an email to{" "}
-				<Link className="text-red"
-					onClick={() => {
-						window.location.href = `mailto:support@stalwartuniversityinstitute.com`;
-					}}
-					to="#"
-				>
+				<a className="text-red" href="mailto:support@stalwartuniversityinstitute.com">
 					support@stalwartuniversityinstitute.com
-				</Link>
+				</a>
 			</p>
 		</div>
 	);
