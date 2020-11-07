944c4d900a46f31262c2db6a4924e2d6b37804d0
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2020-11-07 21:36:41 +0100

    Updated the color of the footer link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
 		<div className="footer container-fluid p-3 p-md-5 bg-blue text-center">
 			<p className="mb-0">
 				Experiencing Technical Difficulty? Call{" "}
-				<a className="text-red" href="tel:0123456789">
+				<a className="" href="tel:0123456789">
 					0123456789
 				</a>{" "}
 				or send an email to{" "}
-				<a className="text-red" href="mailto:support@stalwartuniversityinstitute.com">
+				<a className="" href="mailto:support@stalwartuniversityinstitute.com">
 					support@stalwartuniversityinstitute.com
 				</a>
 			</p>
