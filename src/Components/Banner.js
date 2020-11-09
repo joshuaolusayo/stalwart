c929bb4342ea36fa1c1068d24c9e2c6231c1a0eb
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2020-11-09 11:58:25 +0100

    Updated the responsiveness of the website

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -6,11 +6,11 @@ const Banner = () => {
 			className="banner py-3"
 			style={{ background: `linear-gradient(to right, rgba(0,0,0,.9), rgba(0,0,0,.3)), url("/images/banner.jpg")` }}
 		>
-			<div className="container-fluid">
-				<div className="main d-flex flex-column flex-sm-row justify-content-center justify-content-sm-between align-items-center">
+			<div className="container h-100">
+				<div className="main d-flex flex-column flex-md-row justify-content-center justify-content-md-between align-items-center">
 					<img className="logo" src="/images/logo.png" alt="Logo" loading="lazy" />
 					<a
-						className="btn bg-red text-white px-md-5 py-md-2 shadow-lg mt-3 mt-sm-0"
+						className="btn bg-red text-white px-md-5 py-md-2 shadow-lg mt-3 mt-md-0"
 						href="http://stalwartuniversityinstitute.com/"
 						rel="noreferrer"
 						target="_blank"
@@ -20,7 +20,7 @@ const Banner = () => {
 				</div>
 
 				<div className="row align-items-center banner__row mx-0">
-					<div className="col-sm-10 mx-auto col-lg-6 ml-md-0 px-lg-5">
+					<div className="col-sm-10 col-lg-6 ">
 						<h1 className="font-montserrat">SIS Portal</h1>
 						<p>Welcome to Stalwart University Institute Portal.</p>
 						<p className="font-italic">
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,17 +2,19 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="footer container-fluid p-3 p-md-5 bg-blue text-center">
-			<p className="mb-0">
-				Experiencing Technical Difficulty? Call{" "}
-				<a className="" href="tel:0123456789">
-					0123456789
-				</a>{" "}
-				or send an email to{" "}
-				<a className="" href="mailto:support@stalwartuniversityinstitute.com">
-					support@stalwartuniversityinstitute.com
-				</a>
-			</p>
+		<div className="footer py-3 py-md-5 bg-blue text-center">
+			<div className="container">
+				<p className="mb-0">
+					Experiencing Technical Difficulty? Call{" "}
+					<a className="" href="tel:0123456789">
+						0123456789
+					</a>{" "}
+					or send an email to{" "}
+					<a className="" href="mailto:support@stalwartuniversityinstitute.com">
+						support@stalwartuniversityinstitute.com
+					</a>
+				</p>
+			</div>
 		</div>
 	);
 };
